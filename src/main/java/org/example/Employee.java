ae7f00cf051562e14f1ec90ec28966e015696f9a
Feyzanur-Ak <senturkfeyzanur96@gmail.com>
2025-01-11 11:19:33 +0300

    Employee oluşturuldu

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -2,15 +2,35 @@ package org.example;
 
 public class Employee {
 
-private long id;
-private String fullName;
-private String email;
-private String password;
-private  String [] healthplans;
+ long id;
+ String fullName;
+ String email;
+ String password;
+ String [] healthplans;
 
 public Employee(long id,String fullName,String email,String password,String [] healthplans )
 {
+     this.id=id;
+    this.fullName=fullName;
+    this.email=email;
+    this.password=password;
+    this.healthplans=healthplans;
 
+}
+
+public void addHealthplan(int index, String name)
+{
+
+      if(index<0 || index>=healthplans.length){
+          System.out.println("Dizi boyutunu aşan index");
+          return;
+      }
+
+      if(healthplans[index]==null){
+          healthplans[index]=name;
+      }else{
+          System.out.println("Bu index daha dolu");
+      }
 
 }
 
