5361939d72fda902e6086ec3979e5de009ae4e62
Feyzanur-Ak <senturkfeyzanur96@gmail.com>
2025-01-11 11:54:47 +0300

    Company oluşturuldu

--- src/main/java/org/example/Company.java
+++ src/main/java/org/example/Company.java
@@ -1,4 +1,43 @@
 package org.example;
 
 public class Company {
+    private long id;
+    private String name;
+    private double giro;
+    private String[] developerNames;
+
+    public Company(long id,String name,double giro,String[] developerNames ){
+         this.id=id;
+         this.name=name;
+         this.developerNames=developerNames;
+
+        if(giro<0){
+             this.giro=0;
+         }
+    }
+
+    public void addEmployee(int index, String name) {
+        if(index<0 || index>=developerNames.length){
+            System.out.println("İndex hatalı");
+        }
+
+        if(developerNames[index]==null){
+            developerNames[index]=name;
+        } else{
+            System.out.println("Bu indexte eleman var");
+        }
+    }
+
+   public double getGiro(){
+        return giro;
+   }
+
+    public String[] getDeveloperNames(){
+        return developerNames;
+    }
+
+    public String getName(){
+        return name;
+    }
+
 }
